4b408e83f41b480db14a5b5f6cb8b6b22591421b
Danny Engelman <danny@engelman.nl>
2022-09-02 15:46:25 +0200

    created separate minified file element.min.js

--- element.js
+++ element.js
@@ -1,1 +1,38 @@
-customElements.define("mark-down",class extends HTMLElement{connectedCallback(e=0,i=(e=>"<li>"+e+"</li>")){setTimeout((()=>this.innerHTML=this.innerHTML.replace(/\s\s\n/gim,"\n<br>\n").replace(/\n\n/gim,"\n<br>\n").split("\n").map((e=>e.trim())).join("\n").replace(/^#### (.*$)/gim,"<h4>$1</h4>").replace(/^### (.*$)/gim,"<h3>$1</h3>").replace(/^## (.*$)/gim,"<h2>$1</h2>").replace(/^# (.*$)/gim,"<h1>$1</h1>").replace(/\*\*(.*)\*\*/gim,"<b>$1</b>").replace(/\_\_(.*)\_\_/gim,"<b>$1</b>").replace(/\*(.*)\*/gim,"<i>$1</i>").replace(/\_(.*)\_/gim,"<i>$1</i>").replace(/!\[(.*?)\]\((.*?)\)/gim,"<img alt='$1' src='$2'>").replace(/\[(.*?)\]\((.*?)\)/gim,"<a href='$2'>$1</a>").split(/\n\* /).reduce(((n,l,r)=>{if(e&&l.includes("\n")){let[n,...r]=l.split("\n");l=[i(n),"</ul>",...r].join("\n");e=0}else(e||r&&(e=n.push("<ul>")))&&(l=i(l));n.push(l);return n}),[]).join("\n").concat(e?"</ul>":"\n")))}})
+customElements.define(
+  "mark-down",
+  class extends HTMLElement {
+    connectedCallback(e = 0, i = (e) => "<li>" + e + "</li>") {
+      setTimeout(
+        () =>
+          (this.innerHTML = this.innerHTML
+            .replace(/\s\s\n/gim, "\n<br>\n")
+            .replace(/\n\n/gim, "\n<br>\n")
+            .split("\n")
+            .map((e) => e.trim())
+            .join("\n")
+            .replace(/^#### (.*$)/gim, "<h4>$1</h4>")
+            .replace(/^### (.*$)/gim, "<h3>$1</h3>")
+            .replace(/^## (.*$)/gim, "<h2>$1</h2>")
+            .replace(/^# (.*$)/gim, "<h1>$1</h1>")
+            .replace(/\*\*(.*)\*\*/gim, "<b>$1</b>")
+            .replace(/\_\_(.*)\_\_/gim, "<b>$1</b>")
+            .replace(/\*(.*)\*/gim, "<i>$1</i>")
+            .replace(/\_(.*)\_/gim, "<i>$1</i>")
+            .replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2'>")
+            .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
+            .split(/\n\* /)
+            .reduce((n, l, r) => {
+              if (e && l.includes("\n")) {
+                let [n, ...r] = l.split("\n");
+                l = [i(n), "</ul>", ...r].join("\n");
+                e = 0;
+              } else (e || (r && (e = n.push("<ul>")))) && (l = i(l));
+              n.push(l);
+              return n;
+            }, [])
+            .join("\n")
+            .concat(e ? "</ul>" : "\n"))
+      );
+    }
+  }
+);
