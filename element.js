033dcf36e7aecbb018afa43be43fcb92704ef5c6
Danny Engelman <danny@engelman.nl>
2024-02-05 10:48:06 +0100

    <mark-down> Web Component

--- element.js
+++ element.js
@@ -2,42 +2,44 @@ customElements.define(
   "mark-down",
   class extends HTMLElement {
     connectedCallback(iscode = false) {
-      function markdown(str) {
-        // Extract ``` code blocks and replace with placeholders
+      let markdown = str => {
+      // ===== Extract ``` code blocks and replace with placeholders
         let counter = 0, id, codes = {};
         str = str.replace(/```([\s\S]+?)```/g, (match, code) => {
-          codes[id = "codepre" + counter++] = "<pre><code>" +
-            // trim whitespace and empty lines from codeline
+          codes[id = "precode" + counter++] = "<pre><code>" +
+          // ------------------------------------- trim whitespace and empty lines from codeline
             code.split("\n").map(l => l.trim()).filter(Boolean).join("\n") +
             "</code></pre>";
-          return id; // return placeholder to input str
+          // ------------------------------------- return placeholder to input str
+          return id; 
         });
         return Object.keys(codes).reduce(
           (html, id) => html.replace(id, codes[id]),
-          // process other Markdown tags
+          // --------------------------------------- process other Markdown tags
           [[/#{6}\s?([^\n]+)/g, "<h6>$1</h6>"], // headers from # to ######
             [/#{5}\s?([^\n]+)/g, "<h5>$1</h5>"],
             [/#{4}\s?([^\n]+)/g, "<h4>$1</h4>"],
             [/#{3}\s?([^\n]+)/g, "<h3>$1</h3>"],
             [/#{2}\s?([^\n]+)/g, "<h2>$1</h2>"],
             [/#{1}\s?([^\n]+)/g, "<h1>$1</h1>"],
-      // bold with double ** or __
+            // --------------------------------------- bold with double ** or __
             [/\*\*\s?([^\n]+)\*\*/g, "<b>$1</b>"], // /\*\*(.+?)\*\*/g or /__(.+?)__/g
             [/__([^_]+)__/g, "<b>$1</b>"],
-            // italics with single * or _
+            // --------------------------------------- italics with single * or _
             [/\*\s?([^\n]+)\*/g, "<i>$1</i>"], ///\*(.+?)\*/g or /_(.+?)_/g
             [/_([^_`]+)_/g, "<i>$1</i>"], //
             [/```([\s\S]+?)```/g, '<code><pre>$1</pre></code>'],
-            // paragraphs from newline
+            // --------------------------------------- paragraphs from newline
             [/([^\n]+\n?)/g, "<p>$1</p>"],
-            // UL Lists with + and *
+            // --------------------------------------- UL Lists with + and *
             [/([^\n]+)(\+)([^\n]+)/g, "<ul><li>$3</li></ul>"], // with +
             [/([^\n]+)(\*)([^\n]+)/g, "<ul><li>$3</li></ul>"],// with *
-            // IMGs
+            // --------------------------------------- IMGs
             [/!\[([^\]]+)\]\(([^)]+)\s"([^")]+)"\)/g, '<img src="$2" alt="$1" title="$3">'],
-            // A HREF Links
+            // --------------------------------------- A HREF Links
             [/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>'], // /\[(.+?)\]\((.+?)\)/g
-          ].reduce((html, rule) => html.replace(...rule), str)
+            // --------------------------------------- Process regexs, built HTML
+          ].reduce((html, rgx) => html.replace(...rgx), str)
         );
       }
       setTimeout(() => this.innerHTML = markdown(this.innerHTML));
