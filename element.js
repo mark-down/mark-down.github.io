aa3705b374b57fe21efb135698ad691398a486e5
Danny Engelman <danny@engelman.nl>
2025-03-26 13:38:47 +0100

    Optimized MD code

--- element.js
+++ element.js
@@ -1,72 +1,43 @@
-customElements.define(
-  "mark-down",
-  class extends HTMLElement {
-    connectedCallback(
-      // declare as parameters to save LET/CONST declaration
-      markdown = (
-        // declare as parameters to save LET declaration
-        str,
-        counter = 1, // 0 is 1 more byte in minified code
-        id,
-        codes = {}
-      ) => {
-        // ================================================ Extract ``` code blocks and replace with placeholders
-        str = str.replace(
-          /```([\s\S]+?)```/g,
-          (_, code) => (
-            (codes[(id = "pc" + counter++)] =
-              "<pre><code>" +
-            // ------------------------------------- trim whitespace and empty lines from code line
-              code
-                .split("\n")
-                .map((l) => l.trim())
-                .filter((l) => !!l) // .filter(Boolean) is worse for GZIP size
-                .join("\n") +
-              "</code></pre>"),
-            // ------------------------------------- return placeholder to input str
-            /*return*/ id
-          )
-        );
-        // ================================================ process this.innerHTML with Markdown tags
-        str = Object.keys(codes).reduce(
-          (html, id) => html.replace(id, codes[id]),
-          // --------------------------------------- process other Markdown tags
+customElements.define("mark-down", class extends HTMLElement {
+  connectedCallback() {
+    const md = (
+      str, // input string to convert
+      cnt = 1, id, code = {}) => {
+      // Extract ``` code blocks and replace with placeholders
+      str = str.replace(/```([\s\S]+?)```/g, (_, block) => {
+        code[(id = "c" + cnt++)] =
+          "<pre><code>" +
+        block
+          .split("\n")
+          .map((l) => l.trim())
+            .filter((l) => !!l) + // trim whitespace and empty lines
+          "</code></pre>";
+        return id; // return placeholder
+      });
+      // Process Markdown tags
+      str = Object.keys(code).reduce(
+        (html, id) => html.replace(id, code[id]),
+        [
+          [/#{4}\s?([^\n]+)/g, "<h4>$1</h4>"],
+          [/#{3}\s?([^\n]+)/g, "<h3>$1</h3>"],
+          [/#{2}\s?([^\n]+)/g, "<h2>$1</h2>"],
+          [/#{1}\s?([^\n]+)/g, "<h1>$1</h1>"],
+          [/\*\*([^*]+)\*\*/g, "<b>$1</b>"], // bold with **
+          [/__([^_]+)__/g, "<b>$1</b>"], // bold with __
+          [/\*\s?([^\n]+)\*/g, "<i>$1</i>"], // italics with *
+          [/_([^_`]+)_/g, "<i>$1</i>"], // italics with _
+          [/([^\n]+\n?)/g, "<p>$1</p>"], // paragraphs from newline
+          [/([^\n]+)(\+|\*)([^\n]+)/g, "<ul><li>$3</li></ul>"], // UL Lists with + or *
           [
-            //[/#{6}\s?([^\n]+)/g, "<h6>$1</h6>"],
-            //[/#{5}\s?([^\n]+)/g, "<h5>$1</h5>"], // headers from # to ######
-            [/#{4}\s?([^\n]+)/g, "<h4>$1</h4>"],
-            [/#{3}\s?([^\n]+)/g, "<h3>$1</h3>"],
-            [/#{2}\s?([^\n]+)/g, "<h2>$1</h2>"],
-            [/#{1}\s?([^\n]+)/g, "<h1>$1</h1>"],
-
-            // --------------------------------------- bold with double ** or __
-            [/\*\*([^*]+)\*\*/g, "<b>$1</b>"], // /\*\*(.+?)\*\*/g or /__(.+?)__/g
-            [/__([^_]+)__/g, "<b>$1</b>"],
-            // --------------------------------------- italics with single * or _
-            [/\*\s?([^\n]+)\*/g, "<i>$1</i>"], ///\*(.+?)\*/g or /_(.+?)_/g
-            [/_([^_`]+)_/g, "<i>$1</i>"], //
-            // [/```([\s\S]+?)```/g, "<code><pre>$1</pre></code>"],
-            // --------------------------------------- paragraphs from newline
-            [/([^\n]+\n?)/g, "<p>$1</p>"],
-            // --------------------------------------- UL Lists with + and *
-            [/([^\n]+)(\+)([^\n]+)/g, "<ul><li>$3</li></ul>"], // with +
-            [/([^\n]+)(\*)([^\n]+)/g, "<ul><li>$3</li></ul>"], // with *
-            // --------------------------------------- IMGs
-            [
-              /!\[([^\]]+)\]\(([^)]+)\s"([^")]+)"\)/g,
-              '<img src="$2" alt="$1" title="$3">',
-            ],
-            // --------------------------------------- A HREF Links
-            [/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>'], // /\[(.+?)\]\((.+?)\)/g
-            // --------------------------------------- Process regexes, build HTML str
-          ].reduce((html, rgx) => html.replace(...rgx), str)
-        );
-        // ================================================ return final HTML
-        return str
-      }
-    ) {
-      // wait for lightDOM/innerHTML to be parsed
-      setTimeout(() => (this.innerHTML = markdown(this.innerHTML)));
-    }
+            /!\[([^\]]+)\]\(([^)]+)\s"([^")]+)"\)/g,
+            '<img src="$2" alt="$1" title="$3">',
+          ], // IMGs
+          [/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>'], // A HREF Links
+        ].reduce((html, [rgx, tmpl]) => html.replace(rgx, tmpl), str)
+      );
+      return str; // return final HTML
+    };
+    // Wait for lightDOM/innerHTML to be parsed
+    setTimeout(() => (this.innerHTML = md(this.innerHTML)));
   }
-);
+});
